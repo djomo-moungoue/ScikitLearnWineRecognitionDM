3fddbd88ad8083d672e920dc002ade12a01f8721
djomo-moungoue <djomo.moungoue@gmail.com>
2022-12-01 08:05:19 +0100

    trying to understand how each function works

--- sklearn_analyse_wine_dataset.py
+++ sklearn_analyse_wine_dataset.py
@@ -17,40 +17,49 @@ print("-------------------------------------------------------------------------
 # 1- Charger/importer les données
 
 # Utilisez les jeux de données pour charger le jeu de données intégré sur le vin.
-wine = datasets.load_wine(return_X_y=False, as_frame=False)
+wine_bunch = datasets.load_wine()
+wine_bunch_df = datasets.load_wine(return_X_y=False, as_frame=True)
+wine_data_target = datasets.load_wine(return_X_y=True, as_frame=False) -> X = wine_bunch.data AttributeError: 'tuple' object has no attribute 'data'
+wine_data_target_df = datasets.load_wine(return_X_y=True, as_frame=True) -> X = wine_bunch.data AttributeError: 'tuple' object has no attribute 'data'
+
+
 #print(f"\n---- sklearn wine dataset : START ---\n\n{wine}\n\n---- sklearn wine dataset : END---\n")
 
 # 2- Divisez les données en ensembles de formation et de test
 # Veillez à ce que les données soient divisées de manière aléatoire et que les classes soient équilibrées. 70% des données doivent être utilisées pour la formation.
 
 # Créez les objets X et y pour stocker respectivement les données et la valeur cible.
-X = wine.data
-y = wine.target
+X = wine_bunch.data
+y = wine_bunch.target
 # print(f"\n--- X ---\n {X}")
 # print(f"\n--- y ---\n {y}")
 
 # Vérifier si le jeu de donnée est équilibré ou non. Il revient à vérifier si ses classes sont équilibrées ou non.
 # Déterminer le nombre de classes
-wine_classes = wine.target_names
-print(f"\n--- wine_classes ---\n {wine_classes}") #  ['class_0' 'class_1' 'class_2']
+wine_bunch_classes = wine_bunch.target_names
+print(f"\n--- wine_classes ---\n {wine_bunch_classes}") #  ['class_0' 'class_1' 'class_2']
 
 # Déterminer la répartition des classes
-wine_description = wine.DESCR
-index_from = wine_description.index(":Class Distribution:")+len(":Class Distribution:")
-index_to = wine_description.index(":Creator: R.A. Fisher")
-wine_classes_distribution = wine_description[index_from+1:index_to]
+wine_bunch_description = wine_bunch.DESCR
+index_from = wine_bunch_description.index(":Class Distribution:")+len(":Class Distribution:")
+index_to = wine_bunch_description.index(":Creator: R.A. Fisher")
+wine_bunch_classes_distribution = wine_bunch_description[index_from+1:index_to]
 # print(f"\n--- wine_description ---\n {wine_description}")
-print(f"\n--- wine_classes_distribution ---\n {wine_classes_distribution}") # class_0 (59), class_1 (71), class_2 (48) -> Les classes ne sont pas équilibrées
+print(f"\n--- wine_bunch_classes_distribution ---\n {wine_bunch_classes_distribution}") # class_0 (59), class_1 (71), class_2 (48) -> Les classes ne sont pas équilibrées
 
 
 # Diviser les tableaux ou matrices en sous-ensembles aléatoires de formation et de test.
 # shuffle=True : Assure la répartition des données de manière aléatoire
-#SVC(class_weight='balanced', probability=True) : Assure l'équilibre entre les classes (https://www.analyticsvidhya.com/blog/2020/07/10-techniques-to-deal-with-class-imbalance-in-machine-learning/)
-
+# SVC(class_weight='balanced', probability=True) : Assure l'équilibre entre les classes (https://www.analyticsvidhya.com/blog/2020/07/10-techniques-to-deal-with-class-imbalance-in-machine-learning/)
+# - class_weight : dict ou 'balanced', default=None : Fixe le paramètre C de la classe i à class_weight[i]*C pour le SVC. S'il n'est pas donné, toutes les classes sont supposées avoir un poids de un. 
+# Le mode "équilibré" utilise les valeurs de y pour ajuster automatiquement les poids de manière inversement proportionnelle aux fréquences des classes dans les données d'entrée comme n_échantillons / (n_classes * np.bincount(y)).
+# - random_state : int, RandomState instance ou None, default=None : Contrôle la génération de nombres pseudo-aléatoires pour mélanger les données afin d'estimer les probabilités. Ignoré lorsque la probabilité est False. 
+# Passez un int pour une sortie reproductible sur plusieurs appels de fonction. Voir le glossaire <random_state>.
 svc_balanced_model = SVC(class_weight='balanced', probability=True)
-print(f"\n--- svc_balanced_model ---\n {svc_balanced_model}")
+print(f"\n--- svc_balanced_model ---\n {str(svc_balanced_model)}")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.70, shuffle=True)
+# X : 
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.70, random_state=42, shuffle=True)
 print(f"\n--- X_train len = {len(X_train)} Size = {math.ceil(len(X_train)/len(X)*100)}%  ---")
 print(f"\n--- X_test len = {len(X_test)}  Size = {math.ceil(len(X_test)/len(X)*100)}%   ---")
 print(f"\n--- y_train  len = {len(y_train)}  Size = {math.ceil(len(y_train)/len(y)*100)}%  ---")
@@ -84,5 +93,7 @@ plt.ylabel('Y-Axis')
 plt.xlabel('X-Axis')
 
 plt.legend(["Wine Dataset Classes"])
+
+plt.savefig("ProjectsZa/MachineLearningZa/charts/wine_dataset_barchart.jpg")
 
 plt.show()
